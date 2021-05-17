af54ffac77e910743e65943247c53b866db791cc
Fabian Luttenberger <fabian.luttenberger@htl-wels.at>
2021-05-17 13:51:10 +0200

    Version 1.0

---
+++ src/sample/LoginController.java
@@ -0,0 +1,64 @@
+package sample;
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private VBox vbox;
+    private Parent fxml;
+
+
+
+    @Override
+    public void initialize (URL url, ResourceBundle rb) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(1), vbox);
+        transition.setToX(vbox.getLayoutX() * 20);
+        transition.play();
+        transition.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SignIn.fxml")));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ignored) { }
+        });
+    }
+    @FXML
+    private void open_signin () {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(1), vbox);
+        transition.setToX(vbox.getLayoutX() * 20);
+        transition.play();
+        transition.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SignIn.fxml")));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ignored) { }
+        });
+    }
+    @FXML
+    private void open_signup (){
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(1), vbox);
+        transition.setToX(0);
+        transition.play();
+        transition.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SignUp.fxml")));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch(IOException ignored) { }
+        });
+    }
+
+}
